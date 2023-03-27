57d9893f868cd45e3999f5e408b8cde2cf850b91
Andy Tran <AyyyTran@gmail.com>
2023-03-27 12:04:24 -0700

    adjusted footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import {animateScroll as scroll} from 'react-scroll';
-import {FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin} from 'react-icons/fa';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin,
+} from 'react-icons/fa';
 import {
   FooterContainer,
   FooterLinksContainer,
@@ -27,67 +33,35 @@ const Footer = () => {
   return (
     <FooterContainer>
       <FooterWrap>
-        <FooterLinksContainer>
-          <FooterLinksWrapper>
-            <FooterLinkItems>
-              <FooterLinkTitle> About Us</FooterLinkTitle>
-              <FooterLink to='/'>How It Works</FooterLink>
-              <FooterLink to='/'>Testomonials</FooterLink>
-              <FooterLink to='/'>Careers</FooterLink>
-              <FooterLink to='/'>Investors</FooterLink>
-              <FooterLink to='/'>Terms of Service</FooterLink>
-            </FooterLinkItems>
-            <FooterLinkItems>
-              <FooterLinkTitle> About Us</FooterLinkTitle>
-              <FooterLink to='/'>How It Works</FooterLink>
-              <FooterLink to='/'>Testomonials</FooterLink>
-              <FooterLink to='/'>Careers</FooterLink>
-              <FooterLink to='/'>Investors</FooterLink>
-              <FooterLink to='/'>Terms of Service</FooterLink>
-            </FooterLinkItems>
-          </FooterLinksWrapper>
-          <FooterLinksWrapper>
-            <FooterLinkItems>
-              <FooterLinkTitle> About Us</FooterLinkTitle>
-              <FooterLink to='/'>How It Works</FooterLink>
-              <FooterLink to='/'>Testomonials</FooterLink>
-              <FooterLink to='/'>Careers</FooterLink>
-              <FooterLink to='/'>Investors</FooterLink>
-              <FooterLink to='/'>Terms of Service</FooterLink>
-            </FooterLinkItems>
-            <FooterLinkItems>
-              <FooterLinkTitle> About Us</FooterLinkTitle>
-              <FooterLink to='/'>How It Works</FooterLink>
-              <FooterLink to='/'>Testomonials</FooterLink>
-              <FooterLink to='/'>Careers</FooterLink>
-              <FooterLink to='/'>Investors</FooterLink>
-              <FooterLink to='/'>Terms of Service</FooterLink>
-            </FooterLinkItems>
-          </FooterLinksWrapper>
-        </FooterLinksContainer>
         <SocialMedia>
           <SocialMediaWrap>
-            <SocialLogo to='/' onClick={toggleHome}>
+            <SocialLogo to="/" onClick={toggleHome}>
               BC Nails
             </SocialLogo>
-            <WebsiteRights>BCNails © {new Date().getFullYear()} All rights reserved.</WebsiteRights>
-            <SocialIcons>
-              <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+            <WebsiteRights>
+              BCNails © {new Date().getFullYear()} All rights reserved.
+            </WebsiteRights>
+            {/* <SocialIcons>
+              <SocialIconLink href="/" target="_blank" aria-label="Facebook">
                 <FaFacebook />
               </SocialIconLink>
-              <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+              <SocialIconLink href="/" target="_blank" aria-label="Instagram">
                 <FaInstagram />
               </SocialIconLink>
-              <SocialIconLink href='/' target='_blank' aria-label='Youtube'>
+              <SocialIconLink href="/" target="_blank" aria-label="Youtube">
                 <FaYoutube />
               </SocialIconLink>
-              <SocialIconLink href='//www.twitter.com/username' target='_blank' aria-label='Twitter'>
+              <SocialIconLink
+                href="//www.twitter.com/username"
+                target="_blank"
+                aria-label="Twitter"
+              >
                 <FaTwitter />
               </SocialIconLink>
-              <SocialIconLink href='/' target='_blank' aria-label='Linkedin'>
+              <SocialIconLink href="/" target="_blank" aria-label="Linkedin">
                 <FaLinkedin />
               </SocialIconLink>
-            </SocialIcons>
+            </SocialIcons> */}
           </SocialMediaWrap>
         </SocialMedia>
       </FooterWrap>
